8a4af6c95fd5ef153de5525baefd5fa76f6d8911
Alexandros Koutroulis <icyd3mon@gmail.com>
2021-01-09 14:31:29 +0200

    Initial Commit

---
+++ address-validator.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: WooCommerce Address Validator
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Address Validator for WooCommerce
+Version: 1.0
+Author: Alexandros Koutroulis
+Author URI: https://github.com/mistericy
+License: GPLv3
+Requires at least: 5.6
+Requires PHP: 7.4
+*/
+
+
